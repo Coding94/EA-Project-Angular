8b7036716911f7501016e32f25f0fa946ca58b9f
Giulio <ggiudice94@gmail.com>
2022-02-11 12:59:50 +0100

    feat: finish darkgrey-component card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { DesignSystemComponent } from './design-system/design-system.component';
 import { RedWhiteCardComponent } from './red-white-card/red-white-card.component';
 import { RedCardComponent } from './red-card/red-card.component';
+import { DarkgrayCardComponent } from './darkgray-card/darkgray-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DesignSystemComponent,
     RedWhiteCardComponent,
-    RedCardComponent
+    RedCardComponent,
+    DarkgrayCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/darkgray-card/darkgray-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ea-darkgray-card',
+  templateUrl: './darkgray-card.component.html',
+  styleUrls: ['./darkgray-card.component.css']
+})
+export class DarkgrayCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
