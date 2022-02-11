8052c69384926eae4267b59e47649a62d02d16a5
Giulio <ggiudice94@gmail.com>
2022-02-11 12:49:36 +0100

    feat: create red card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DesignSystemComponent } from './design-system/design-system.component';
 import { RedWhiteCardComponent } from './red-white-card/red-white-card.component';
+import { RedCardComponent } from './red-card/red-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DesignSystemComponent,
-    RedWhiteCardComponent
+    RedWhiteCardComponent,
+    RedCardComponent
   ],
   imports: [
     BrowserModule
