88a4f8594310eb8d134456b1a409fc1793f62790
Giulio <ggiudice94@gmail.com>
2022-02-18 21:39:34 +0100

    feat: finish battlefield example card animation

--- src/app/square-effect-card/square-effect-card.component.ts
+++ src/app/square-effect-card/square-effect-card.component.ts
@@ -6,11 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./square-effect-card.component.css'],
 })
 export class SquareEffectCardComponent implements OnInit {
+  translateY: any;
+  transitionmS: any;
+  prova: any = false;
+  opacita: any = 0.1;
+  transizione: any = 'opacity 0.6s';
+
   constructor() {}
 
   ngOnInit(): void {}
 
-  funzioneHover() {
-    console.log('ciao a tutti 2 ');
+  hoverFunction() {
+    this.translateY = 'translateY(-60px)';
+    this.transitionmS = 'all 300ms ease-out';
+    this.prova = true;
+    console.log(this.prova);
+    this.opacita = '1';
+    this.transizione = 'opacity 0.6s';
+  }
+  outFunction() {
+    this.translateY = 'translateY(0)';
+    this.transitionmS = 'all 300ms ease-out';
+    this.prova = false;
+    console.log(this.prova);
+    this.opacita = '0';
+    this.transizione = 'opacity 0.1ms';
   }
 }
