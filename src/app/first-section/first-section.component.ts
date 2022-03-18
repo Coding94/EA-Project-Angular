aa230a03e956c92b86bab0dc9a42bc595a746bb8
Costantino92 <dragos.ctin92@gmail.com>
2022-03-18 10:44:53 +0100

    remove cards animation

--- src/app/first-section/first-section.component.ts
+++ src/app/first-section/first-section.component.ts
@@ -9,47 +9,47 @@ export class FirstSectionComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    this.cardAnim();
+    // this.cardAnim();
   }
-  cardAnim() {
-    let divBgImg: any = document.querySelector('.bgImg');
-    let btnTwo: any = document.querySelector('.bttn-two');
+  // cardAnim() {
+  //   let divBgImg: any = document.querySelector('.bgImg');
+  //   let btnTwo: any = document.querySelector('.bttn-two');
 
-    divBgImg.addEventListener('mouseover', () => {
-      divBgImg.firstElementChild.style.transform =
-        'translateY(-200px) scale(1.3)';
-      divBgImg.firstElementChild.style.transition = 'ease 0.5s';
-      btnTwo.style.opacity = '1';
-    });
+  //   divBgImg.addEventListener('mouseover', () => {
+  //     divBgImg.firstElementChild.style.transform =
+  //       'translateY(-200px) scale(1.3)';
+  //     divBgImg.firstElementChild.style.transition = 'ease 0.5s';
+  //     btnTwo.style.opacity = '1';
+  //   });
 
-    divBgImg.addEventListener('mouseout', () => {
-      divBgImg.firstElementChild.style.transform = 'translateY(0px) scale(1.3)';
-      divBgImg.firstElementChild.style.transition = 'ease 0.5s';
-      btnTwo.style.opacity = '0';
-    });
+  //   divBgImg.addEventListener('mouseout', () => {
+  //     divBgImg.firstElementChild.style.transform = 'translateY(0px) scale(1.3)';
+  //     divBgImg.firstElementChild.style.transition = 'ease 0.5s';
+  //     btnTwo.style.opacity = '0';
+  //   });
 
-    const battlefieldCard: any = document.querySelectorAll('.battleFieldCard');
+  //   const battlefieldCard: any = document.querySelectorAll('.battleFieldCard');
 
-    for (let i = 0; i < battlefieldCard.length; i++) {
-      const battlefieldLogo: any =
-        document.querySelectorAll('.battleFieldLogo');
-      const gradientColor: any = document.querySelectorAll('.gradientColor');
+  //   for (let i = 0; i < battlefieldCard.length; i++) {
+  //     const battlefieldLogo: any =
+  //       document.querySelectorAll('.battleFieldLogo');
+  //     const gradientColor: any = document.querySelectorAll('.gradientColor');
 
-      battlefieldCard[i].addEventListener('mouseover', animCard);
-      battlefieldCard[i].addEventListener('mouseout', disAnimCard);
+  //     battlefieldCard[i].addEventListener('mouseover', animCard);
+  //     battlefieldCard[i].addEventListener('mouseout', disAnimCard);
 
-      function animCard() {
-        battlefieldLogo[i].style.transform = 'translateY(-60px)';
-        battlefieldLogo[i].style.transition = 'all 300ms ease-out;';
-        gradientColor[i].style.opacity = '1';
-        gradientColor[i].style.color = 'white';
-      }
+  //     function animCard() {
+  //       battlefieldLogo[i].style.transform = 'translateY(-60px)';
+  //       battlefieldLogo[i].style.transition = 'all 300ms ease-out;';
+  //       gradientColor[i].style.opacity = '1';
+  //       gradientColor[i].style.color = 'white';
+  //     }
 
-      function disAnimCard() {
-        battlefieldLogo[i].style.transform = 'translateY(0)';
-        battlefieldLogo[i].style.transition = 'all 300ms ease-out;';
-        gradientColor[i].style.opacity = '0';
-      }
-    }
-  }
+  //     function disAnimCard() {
+  //       battlefieldLogo[i].style.transform = 'translateY(0)';
+  //       battlefieldLogo[i].style.transition = 'all 300ms ease-out;';
+  //       gradientColor[i].style.opacity = '0';
+  //     }
+  //   }
+  // }
 }
